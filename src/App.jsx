c9123f203515183e0927e2e10c239ea4436ec769
wisneysouza <wisney.souza@gmail.com>
2025-02-07 10:36:35 -0300

    inicio da tela de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+
+import Loggin from "./components/loggin/Loggin.jsx";
 function App(){
   return(
     <>
     <h1>SISPAR</h1>
+    <Loggin/>
     </>
   )
 
---
+++ src/components/loggin/Loggin.jsx
@@ -0,0 +1,38 @@
+import Logo from "../../assets/Tela <Login />
+<logo-ws className="png"></logo-ws>."
+function Loggin(){
+    return(
+        <>
+        <main>
+            <section>
+                backgroud 
+            </section>
+
+            <section>
+                <img src={Logo} alt="Logo da Wilson Sons" />
+
+                <h1></h1>
+
+                <h2></h2>
+
+                <p></p>
+
+                <form action="">
+                    <input type="email" name="email" id="email" placeholder="Email"/>
+                    <input type="password" name="email" id="email" placeholder="Senha" />
+
+                   <a href=""> </a>
+                   <button></button>
+                   <ton>
+        
+
+                </form>
+
+            </section>
+        </main>
+        
+        </>
+    )
+} 
+
+export default Loggin
